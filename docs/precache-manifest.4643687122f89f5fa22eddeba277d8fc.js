987ec3488f2165b4d782df84e3740dd3660962ef
Sebastián Simunovic <ssimunovic02@gmail.com>
2020-09-26 11:58:17 -0300

    Primer Commit

---
+++ docs/precache-manifest.4643687122f89f5fa22eddeba277d8fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f41ad4765de3bbd0c1ccfb216b47e5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bcf25a9472d1442fdcc",
+    "url": "/static/css/main.fdca9178.chunk.css"
+  },
+  {
+    "revision": "01ee07fce66f1e5c8202",
+    "url": "/static/js/2.28e260f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.28e260f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bcf25a9472d1442fdcc",
+    "url": "/static/js/main.2b6b961b.chunk.js"
+  },
+  {
+    "revision": "9ba425a0e05793b2aa09",
+    "url": "/static/js/runtime-main.b4aef87d.js"
+  },
+  {
+    "revision": "96e45d694833f161c9dc02ab9dde4a6b",
+    "url": "/static/media/cryptomonedas.96e45d69.png"
+  }
+]);
